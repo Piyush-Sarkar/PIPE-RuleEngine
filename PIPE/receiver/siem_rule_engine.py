f87e26bb87f4108165c1f1f03be1e02545c47fa9
PIYUSH SARKAR <142200345+Piyush-Sarkar@users.noreply.github.com>
2026-04-27 13:09:30 +0530

    Update siem_rule_engine.py

--- PIPE/receiver/siem_rule_engine.py
+++ PIPE/receiver/siem_rule_engine.py
@@ -403,4 +403,4 @@ async def process_logs(request: Request):
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5002)
+    uvicorn.run(app, host="127.0.0.1", port=5001)
